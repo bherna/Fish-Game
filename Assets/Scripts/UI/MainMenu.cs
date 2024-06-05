ad821447b2e658b8c7001a1657a4c642d30899f9
bherna <bherna153@gmail.com>
2024-06-04 18:28:33 -0600

    readme update

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
+    [SerializeField] List<Transform> Panels;
+    [SerializeField] int rectTransform_width = 1920;
+
+    private void Start() {
+
+        for (int i = 0; i <= Panels.Count-1; i++){
+            Panels[i].transform.localPosition = new Vector3(rectTransform_width*i,0,0);
+        }
+        
+
+    }
     public void GoToScene(string sceneName){
 
         SceneManager.LoadScene(sceneName);
@@ -12,5 +24,21 @@ public class MainMenu : MonoBehaviour
     public void QuitApp(){
         Application.Quit();
         Debug.Log("application has quit.");
+    }
+
+    public void Next_UIScreen(){
+
+        foreach(Transform panel in Panels){
+
+            panel.transform.localPosition -= new Vector3(rectTransform_width,0,0);
+        }
+    }
+
+    public void Previous_UIScreen(){
+
+        foreach(Transform panel in Panels){
+
+            panel.transform.localPosition += new Vector3(rectTransform_width,0,0);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Will update this when I have an idea of what i'm doing for this game.
+Creating a insaniquarium inspired game.
+
+![](gameplay.GIF)
+
+Raise fish to collect their coins as they mature. Occasionally enemies will spawn into the tank to eat your fish.
+Shoot them down to keep them from eating your fish. 
