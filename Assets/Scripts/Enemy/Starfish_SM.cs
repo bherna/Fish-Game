1d673a5dee774b6d7a40e0ac2c2eda3865919700
bherna <bherna153@gmail.com>
2024-06-27 19:01:00 -0700

    idk

---
+++ Assets/Scripts/Enemy/Starfish_SM.cs
@@ -0,0 +1,61 @@
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Starfish_SM : MonoBehaviour, IPointerClickHandler
+{
+
+    [SerializeField] Transform sprite;
+    [SerializeField] float startSize = 1;
+
+    [SerializeField] float endSize = 3;
+
+    [SerializeField] float rateOfGrowth = 1;
+    private int health = 4;
+
+    private float currSize = 0;
+
+    private void Start() {
+        currSize = startSize;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(currSize < endSize){
+
+            currSize += Time.deltaTime * rateOfGrowth;
+
+            sprite.localScale = new Vector3(currSize, currSize, 1);
+        }
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData){
+
+
+        //if the game is paused, return
+        if(Controller_Main.instance.paused){
+            return;
+        }
+
+        //damage
+        health -= 5;
+
+        //die
+        if(health <= 0){
+            Died();
+        }
+        
+    }
+
+    //fish died
+    //will be more than one way to die
+    private void Died(){
+
+        //remove the enemy from list
+        Controller_Enemy.instance.CloserToWaveEnded();
+
+        //die
+        Destroy(gameObject);
+    }
+}
