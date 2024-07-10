839e39c607a2ff424bec5bf73669876bac9e26ea
bherna <bherna153@gmail.com>
2024-07-10 01:10:32 -0700

    created gun animation + created gun particl + created player controller

---
+++ Assets/Scripts/Controller_S/Controller_Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Controller_Player : MonoBehaviour
+{
+    
+    [SerializeField] ParticleSystem Gun_particle;
+
+    //used for getting mouse position (what is our target z axis) (is in the bg-level gameobject)
+    [SerializeField] Transform targetZ;
+
+    public static Controller_Player instance {get; private set; }
+
+    private void Awake() {
+        
+        //delete duplicate of this instance
+
+        if (instance != null && instance != this){
+            Destroy(this);
+        }
+        else{
+            instance = this;
+        }
+    } 
+
+    private void Update() {
+        
+        if(Input.GetMouseButtonDown(0)){
+
+            var screenPos = Input.mousePosition;
+            screenPos.z = Vector3.Dot(Camera.main.transform.forward, targetZ.position - Camera.main.transform.position);
+            var mousePos = Camera.main.ScreenToWorldPoint(screenPos); 
+            Instantiate(Gun_particle, mousePos, Quaternion.identity);
+        }
+    }
+}
