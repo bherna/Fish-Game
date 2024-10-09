783b1798a9f4c08465e80acefd9e1f256cc3e37e
bherna <bherna153@gmail.com>
2024-10-09 16:05:24 -0700

    updating khalid pet, so far idle and polyp mode are created, still need protect mode , and testing

---
+++ Assets/Scripts/Pets/Pet_Khalid.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+/// <summary>
+/// 
+//    Desc: An immortal jellyfish, Khalid is your never dieing best friend that you can always rely on once per enemy wave
+//    Ability:  When ever a new enemy wave starts, Khalid will become one target that enemies can go after
+//              If Khalid dies from an enemy, he goes into his polyp mode
+//              In Polyp mode, he is untargetable and slowly regains his health, once healed he is back to being targetable
+//    Rarity: Level Questing
+//
+/// </summary>
+/// 
+
+
+
+public class Pet_Khalid : Pet_ParentClass
+{
+
+
+    // --------------------- stats ------------------------- //
+    private int curr_health = 0;
+    private const int max_health = 20; 
+
+
+    //
+
+    private float curr_seconds = 0; //used in both protect mode and polyp mode, reset as needed
+    private const float secondsInPolyp = 10;
+    private Vector2 bot_of_tank = new Vector2(0, 4.5f);
+    private Event_Type event_type = Event_Type.enemyWave;
+    private bool inPolyp = false;
+
+    // Start is called before the first frame update
+    private new void Start()
+    {
+        base.Start();
+
+        curr_health = max_health;
+    }
+
+    // Update is called once per frame
+    private new void Update()
+    {
+        base.Update();
+
+        switch(curr_PetState){
+
+            case Pet_States.idle:
+                IdleMode();
+                break;
+            case Pet_States.protect:
+                ProtectMode();
+                break;
+            case Pet_States.ability:
+                PolypMode();
+                break;
+            
+            default:
+                Debug.Log(gameObject.ToString() + " currently not in state.");
+                break;
+        }
+    }
+
+
+    public void TakeDamage(int damage){
+        curr_health -= damage;
+
+        if(curr_health <= 0){
+            curr_PetState = Pet_States.ability;
+
+            //reset our seconds, 
+            curr_seconds = 0;
+
+            //become untargetable------------------------------------------------------------------------------------------------------------------
+
+        }
+    }
+
+    //in protect mode, periodically send a message to the first enemy in the controller_enemy list
+    //make them target us
+    //
+    private void ProtectMode(){
+
+    }
+
+
+    //enter hibernating  mode essentially
+    //we are dead pretty much and need to regain health
+    private void PolypMode(){
+
+        // --------------------------- heading to bottom of tank code -------------------------- //
+        var distance = Vector3.Distance(bot_of_tank, transform.position);
+
+        if(Mathf.Abs(distance) > targetRadius){
+            
+            updatePosition(idleTarget, idle_velocity);
+            return;
+        }
+        //animation related
+        //we we didn't start the polyp anitaion, we done it once here
+        else if(!inPolyp){
+            //udate animtaion
+        }
+        //else we are close enough
+        //start polyp mode
+        // ------------------------------------- in polyp ----------------------------------- //
+        
+        //update timer
+        curr_seconds += Time.deltaTime;
+        
+
+        if(curr_seconds >= secondsInPolyp){
+
+            //we are reborn
+            curr_health = max_health;
+            curr_PetState = Pet_States.idle; //bug ?,    rightnow we can only die once per wave, since we can't re-enter protect mode from middle of wave
+
+            //update animation
+
+            //reset curr seconds
+            curr_seconds = 0;
+        }
+
+    }
+
+
+    //when ever a new wave starts, khalid becomes targetable for enemies
+    //ender protect mode
+    public override void Event_Init(Event_Type type, GameObject obj){
+
+        //if we didn't get our event type, return
+        //if current pet state is set to ability (ie: we are hibernating ), return
+        if(type != event_type ||
+            curr_PetState == Pet_States.ability
+        ){return;}
+        
+        //
+        curr_PetState = Pet_States.protect;
+        curr_seconds = 0; //reset this in case
+
+        //animation updated
+    }
+    //wave ends, return to idle
+    public override void Event_EndIt(Event_Type type){
+        //if we didnt get our event type, return
+        //if we are NOT currently in protect mode, return
+        if(type != event_type || 
+            curr_PetState != Pet_States.protect){return;}
+
+        curr_PetState = Pet_States.idle;
+        
+        //animation updated
+    }
+}
