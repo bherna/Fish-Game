adaece7da0e27876fd24826c725518fc24c1d2bc
bherna <bherna153@gmail.com>
2024-11-08 22:24:47 -0700

    added khalid sprites to khalid pet prefab, works so faar, just need to add the animation triggers, also need to fix khalid showing up infront of the tank (should be buried in the gravel kind of), also maybe might add different faces, to show damage level on khalid, since we cant tell other wise until dead

--- Assets/Scripts/Controller_S/Controller_Pets.cs
+++ Assets/Scripts/Controller_S/Controller_Pets.cs
@@ -71,8 +71,8 @@ public class Controller_Pets : MonoBehaviour
         //pet_list.Add(Instantiate(Resources.Load("Pets/" + "Pet_SchoolTeacher") as GameObject, Vector2.zero, quaternion.identity));
         //pet_list.Add(Instantiate(Resources.Load("Pets/" + "Pet_DrCrabs") as GameObject, Vector2.zero, quaternion.identity));
         //pet_list.Add(Instantiate(Resources.Load("Pets/" + "Pet_MaryFertile") as GameObject, Vector2.zero, quaternion.identity));
-        //pet_list.Add(Instantiate(Resources.Load("Pets/" + "Pet_Khalid") as GameObject, Vector2.zero, quaternion.identity));
-        pet_list.Add(Instantiate(Resources.Load("Pets/" + "Pet_Salt") as GameObject, Vector2.zero, quaternion.identity));
+        pet_list.Add(Instantiate(Resources.Load("Pets/" + "Pet_Khalid") as GameObject, Vector2.zero, quaternion.identity));
+        //pet_list.Add(Instantiate(Resources.Load("Pets/" + "Pet_Salt") as GameObject, Vector2.zero, quaternion.identity));
     }
 
 
