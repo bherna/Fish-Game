bd446f50a0b77039272861acd6a5850b9e4c945d
bherna <bherna153@gmail.com>
2024-11-26 12:51:12 -0700

    created a main menu swimming version of the cherry, added cherry to the newgameDictoinary, updated petaccess script to now make sure to check for new pets that are added to the game (OLD: there is a local save file that keeps track of players unlocked pets, but doesnt have a reference to any new future pets)

--- Assets/Scripts/save and load/PetsAccess.cs
+++ Assets/Scripts/save and load/PetsAccess.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 
-public enum PetNames {SchoolTeacher, DrCrabs, WhiteKnight, TinyOctopus, Athos, Porthos, Aramis, Khalid, MaryFertile, Salt, Missing}
+public enum PetNames {SchoolTeacher, DrCrabs, WhiteKnight, TinyOctopus, Athos, Porthos, Aramis, Khalid, MaryFertile, Salt, Cherry, Missing}
 
 public static class PetsAccess 
 {
@@ -12,6 +12,26 @@ public static class PetsAccess
     //each pet is an index in the array
     public static Dictionary<PetNames, bool> petAccess; //entire dictionary list of all pets in game, and their accessability
     public static List<PetNames> current_pets_slotted; //pets the player wants to use next tank
+
+
+    // ONLY EDIT THIS DICTIONARY IF YOU WANT TO ADD A NEW PET
+    // * whenever a new pet is added to our resources folder, we then need to add a reference to it here (that should be it)
+    // * the ordering matches how they are displayed in the main menu pet selection screen
+    // * ALSO MAKE SURE WE HAVE A "./resources/mainmenu/petswim/{PetName}" obj reference to show (you can duplicate the missing prefab, just change the name) 
+    private static Dictionary<PetNames, bool> newGamePetsDictionary = new Dictionary<PetNames, bool>{
+            {PetNames.SchoolTeacher, false},
+            {PetNames.DrCrabs, false},
+            {PetNames.WhiteKnight, false},
+            {PetNames.Cherry, false},
+            {PetNames.TinyOctopus, false},
+            {PetNames.Athos, false},
+            {PetNames.Porthos, false},
+            {PetNames.Aramis, false},
+            {PetNames.Khalid, false},
+            {PetNames.MaryFertile, false},
+            {PetNames.Salt, false}
+
+        };
 
     //this should be the default method to be used in any loading
     //pull any saved data loaded within the save path for pet access, if non, create new game pet access
@@ -23,9 +43,7 @@ public static class PetsAccess
             Pets_Data_Serializable data = SaveLoad.Load_Pets();
             
             if(data != null){
-                //then load Pets
-                petAccess = data.pets;
-                current_pets_slotted = data.curr_pets;
+                LoadGame(data);
             }
             else{
                 //then we are a new game pets dictionary
@@ -40,24 +58,53 @@ public static class PetsAccess
     public static void NewGame(){
 
         //Debug.Log("New Pets Dictionary + collection");
-
-        petAccess = new Dictionary<PetNames, bool>{
-            {PetNames.SchoolTeacher, false},
-            {PetNames.DrCrabs, false},
-            {PetNames.WhiteKnight, false},
-            {PetNames.TinyOctopus, false},
-            {PetNames.Athos, false},
-            {PetNames.Porthos, false},
-            {PetNames.Aramis, false},
-            {PetNames.Khalid, false},
-            {PetNames.MaryFertile, false},
-            {PetNames.Salt, false}
-
-        };
-
-
+        petAccess = newGamePetsDictionary;
+        
+        //reset slotted pets, else player can have pets preselected pets to take into game
         current_pets_slotted = new List<PetNames>();
 
+    }
+
+
+    //load game takes the data we are give, assuming the data is not null
+    //if the pet dictionary size is different from what was loaded, then we have updated our game for new pets
+    //so will need to take that into consideration ation
+    private static void LoadGame(Pets_Data_Serializable data){
+
+        //if pet dictionarys are same size, then we can just load normally
+        if(data.pets.Count == newGamePetsDictionary.Count){
+
+            petAccess = data.pets;
+            current_pets_slotted = data.curr_pets;
+        }
+        //if our save has a smaller size, then we have to update our saved pets dict to match new pets dict
+        else if(data.pets.Count < newGamePetsDictionary.Count){
+
+            var newPetsAccess = newGamePetsDictionary;
+            
+            //for each pet in our currently loaded pet dictionary
+            foreach(KeyValuePair<PetNames, bool> oldPetEntry in data.pets){
+                //if our old pet entry is unlocked
+                if(oldPetEntry.Value){
+                    //then update our new pet dictionary to have it unlocked as well
+                    newPetsAccess[oldPetEntry.Key] = true;
+                }
+                //else it stays locked
+            }
+
+            //now we have a new dictionary
+            petAccess = newPetsAccess;
+            //also reset slotted pets, just in case
+            current_pets_slotted = new List<PetNames>();
+        }
+        //else our loaded pets is bigger than our new dictionary
+        //which should not be possible
+        else{
+
+            //Send debug message, and set our pets unlock to be empty.
+            Debug.Log(string.Format("There are more pets loaded than possible.")); 
+            NewGame();  
+        }
     }
 
 
