b6a7e586df2a756586388ae93a587ee74c16330b
bherna <bherna153@gmail.com>
2025-04-25 19:38:43 -0700

    khalid reqs works now

--- Assets/Scripts/TankScene/Requirements/PetReq_Khalid.cs
+++ Assets/Scripts/TankScene/Requirements/PetReq_Khalid.cs
@@ -2,17 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PetReq_Khalid : MonoBehaviour
+public class PetReq_Khalid : PetReq_ParentClass
 {
-    // Start is called before the first frame update
-    void Start()
+    
+
+    //khalid's egg whats adult guppys to be sacrificed during en enemy wave
+    private int ADeaths = 0;
+    private const int ADeaths_req = 10;
+
+
+     public override void StartReqs()
     {
-        
+        base.StartReqs();
+
+        //update parent variables
+        income_req = 150;
+
+        //update requiremtns board + checker (last)
+        PostUpdates();
+
+        //start the income caller, since we want to keep track of that stuff
+        StartCoroutine(Controller_Wallet.instance.CalculateIncome());
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    //everytime a new update to our reqs changes, we update the visuals, and we chceck to see if we completed reqs
+    protected override void PostUpdates(){
+
+        if(!toggle){return;}
+
+        string ourTex = string.Format("Requirements:\nAdult Deaths during Enemy Wave: {0} / {1}\nIncome: ${2} / ${3}", ADeaths, ADeaths_req, income, income_req);
+        Controller_Requirements.instance.UpdateReqs(ourTex);
+
+
+        //did we complete our reqs
+        if(ADeaths >= ADeaths_req && income >= income_req){
+            
+            //we done, and we can stop
+            toggle = false;
+
+            //run post game thingy
+            Controller_Objective.instance.LevelComplete();
+
+        }
+
     }
+
+
+
+    //we only care if adults (or anything above an adult) dies during an enemy wave so
+    public override void UpdateGuppyCounter_Age(int age, int val){
+
+        if(age >= 2 && val == -1 && Controller_Enemy.instance.currently_in_wave){
+
+            ADeaths++;
+        }
+
+        
+        //update requiremtns board + checker
+        PostUpdates();
+
+                
+    }
+
+
 }
